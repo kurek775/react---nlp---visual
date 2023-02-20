88944d1702a98f960e965b543d4ae6998353139a
Pavel Kurek <kurp03@vse.cz>
2023-02-20 01:18:32 +0100

    - fourth

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.post("/api", (req, res) => {
 
-    const textRazor = new TextRazor('5931c6c93f9a0efae43e3a2b08d3005b71245e1e6f2679221cd1825f')
+    const textRazor = new TextRazor('<API-KEY>')
     const content = req.body.data
     const options = {extractors: 'entities'}
     textRazor.exec(content, options)
